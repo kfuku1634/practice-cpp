f86a606e3d18b0dce5271e65aad34d321fe1cae5
kfuku1634 <kfuku1634.dev@gmail.com>
2022-11-02 08:56:57 +0900

    implement push method.

--- priority_queue/priority_queue.h
+++ priority_queue/priority_queue.h
@@ -1,16 +1,26 @@
+#include "vector/vector.h"
 
 namespace kf {
+    template <typename T>
     class priority_queue {
         public:
-            priority_queue();
-            int size();
-            int capacity();
-            bool empty();
-            void push(int);
-            int pop();
+            priority_queue(){}
+            int size(){ return vec.size(); }
+            bool empty(){ return vec.empty(); }
+            int capacity(){ return vec.capacity(); }
+            void push( T element ){ 
+                int insert_index=0;
+                while( insert_index != vec.size() ){
+                    if( vec.at(insert_index) >  element ){
+                        break;
+                    }
+                    insert_index++;
+                }
+                vec.insert(insert_index, element);
+            }
+            void pop(){ vec.pop(); }
+            T &top(){ return vec.back(); }
         private:
-            int *data;
-            int capacity_;
-            int size_;
+            vector<T> vec;
     };
 }
--- priority_queue/priority_queue_test.cc
+++ priority_queue/priority_queue_test.cc
@@ -1,12 +1,24 @@
 #include <gtest/gtest.h>
 #include "priority_queue.h"
 
-TEST(PriorityQueueTest, PriorityQueueEmpty) {
-  kf::priority_queue pq;
+TEST(PriorityQueueTest, PriorityQueueInt) {
+  kf::priority_queue<int> pq;
   EXPECT_EQ(pq.size(), 0);
-  EXPECT_EQ(pq.empty(), true);
+  EXPECT_TRUE(pq.empty());
   pq.push(10);
-  EXPECT_EQ(pq.pop(), 10);
+  EXPECT_EQ(pq.top(), 10);
   EXPECT_EQ(pq.capacity(), 1);
+  EXPECT_EQ(pq.size(), 1);
+  EXPECT_FALSE(pq.empty());
+
+  pq.push(20);
+  EXPECT_EQ(pq.top(), 20);
+  EXPECT_EQ(pq.size(), 2);
+  EXPECT_EQ(pq.capacity(), 2);
+
+  pq.push(5);
+  EXPECT_EQ(pq.top(), 20);
+  EXPECT_EQ(pq.size(), 3);
+  EXPECT_EQ(pq.capacity(), 4);
 
 }
